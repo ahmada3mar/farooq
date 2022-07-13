2d6ed383f77ab3904b52c3940909ea11c4dbec65
mhmdbdour <mhmdobdour@gmail.com>
2022-07-13 11:26:42 +0300

    profile update

---
+++ public/js/resources_js_Pages_Profile2_test_txt.js
@@ -0,0 +1,13 @@
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["resources_js_Pages_Profile2_test_txt"],{
+
+/***/ "./resources/js/Pages/Profile2/test.txt":
+/*!**********************************************!*\
+  !*** ./resources/js/Pages/Profile2/test.txt ***!
+  \**********************************************/
+/***/ (() => {
+
+
+
+/***/ })
+
+}]);
